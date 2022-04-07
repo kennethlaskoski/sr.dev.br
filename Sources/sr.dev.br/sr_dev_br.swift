8f7aac941caa65c80c07db6597bd9f91d2532bf8
Kenneth Laskoski <kennethlaskoski@gmail.com>
2022-04-07 00:03:22 -0300

    Created Swift Package project

---
+++ Sources/sr.dev.br/sr_dev_br.swift
@@ -0,0 +1,6 @@
+public struct sr_dev_br {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/sr.dev.brTests/sr_dev_brTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import sr_dev_br
+
+final class sr_dev_brTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(sr_dev_br().text, "Hello, World!")
+    }
+}
