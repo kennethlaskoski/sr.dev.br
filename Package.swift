2b83b23a9412421d47ed1ec99c81d270480b2e19
Kenneth Laskoski <kennethlaskoski@gmail.com>
2022-04-07 02:06:17 -0300

    Added ECHO server code

--- Package.swift
+++ Package.swift
@@ -12,12 +12,17 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+      .package(url: "https://github.com/apple/swift-nio.git", .upToNextMajor(from: "2.0.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
-            name: "Server"),
+            name: "Server",
+            dependencies: [
+              .product(name: "NIOPosix", package: "swift-nio"),
+              .product(name: "NIOCore", package: "swift-nio"),
+            ]),
         .testTarget(
             name: "ServerTests",
             dependencies: ["Server"]),
